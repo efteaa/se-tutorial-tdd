6b018be0f863d3b575766301b3287f29765ccf54
efteaa <felice.tania@binus.ac.id>
2025-05-08 12:11:38 +0700

    Update operations.py

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,17 +1,13 @@
 def add(a, b):
     return a + b
 
-
 def subtract(a, b):
-    # TODO: implement this
-    pass
-
+    return a - b
 
 def multiply(a, b):
-    # TODO: implement this
-    pass
-
+    return a * b
 
 def divide(a, b):
-    # TODO: implement this (don't forget to handle division by zero)
-    pass
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
